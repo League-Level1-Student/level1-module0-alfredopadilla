6317faf633a75106073506f1ea9997ae4d9294e4
ComputerCodingMaster <hellothereobiwan1@gmail.com>
2019-08-03 11:27:21 -0700

    Finished FortuneCookie and started Jack in the box
    I completed fortune cookie, and started jack in the box.

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,15 +1,46 @@
-public class FortuneCookie {
-	public static void main(String[] args) {
-		showButton();
-		{
-			System.out.println("Button clicked");
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class FortuneCookie implements ActionListener {
+	public FortuneCookie() {
+	}
+
+	public void showButton() {
+		System.out.println("Button clicked");
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JButton button = new JButton();
+		frame.add(button);
+		frame.pack();
+		button.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, "Woohoo!");
+		int rand = new Random().nextInt(5);
+		if (rand == 0) {
+			JOptionPane.showMessageDialog(null, "You won't find love! :(");
 		}
+		if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "You won't be lonely! :)");
+		}
+		if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "You'll win the lottery! :)");
+		}
+		if (rand == 3) {
+			JOptionPane.showMessageDialog(null, "Be careful where you step! :|");
+		}
+		if (rand == 4) {
+			JOptionPane.showMessageDialog(null, "You will lose something you value! :(");
+		}
+
 	}
 
-	static void showButton() {
-	
-	
-		
-
-	}
 }
---
+++ src/Jackinthebox.java
@@ -0,0 +1,14 @@
+
+public class Jackinthebox {
+
+	public static void main(String[] args) {
+	}
+
+	public Jackinthebox() {
+
+	}
+
+	public void showSuprise() {
+
+	}
+}
