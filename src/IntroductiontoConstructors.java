cbe1247d19231299f0e4d24c2e69e4eff2f58336
League <league@Mac-mini-4.local>
2019-12-21 11:25:06 -0800

    Completing Intro to Constructors

---
+++ src/IntroductiontoConstructors.java
@@ -0,0 +1,11 @@
+public class IntroductiontoConstructorsRunnerClass {
+}
+    public static void main(String [] args){
+        Platypus PlatypusObject= new Platypus("Joseph");
+        Platypus PlatypusObject2= new Platypus("Cannon");
+        PlatypusObject.sayHi();
+        PlatypusObject2.sayHi();
+
+
+
+    }
---
+++ src/IntroductiontoConstructorsRunnerClass.java
@@ -0,0 +1,26 @@
+class Platypus{
+
+public class IntroductiontoConstructors {
+}
+
+private String PlatypusName;
+    public Platypus(String name){
+        PlatypusName= name;
+    }
+    public void setName (String name) {
+        PlatypusName = name;
+    }
+    public String getPlatypusName() {
+        return PlatypusName;
+    }
+    public void name(){
+        System.out.printf("The platypus \" + name + \" is smarter than your average platypus");
+
+
+
+
+
+
+
+    }
+}
